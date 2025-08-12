db93ce4778439d1fcde621b5bda0791ccf7c2706
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2025-08-12 10:16:28 -0500

    feat: add getProductInventory method to ProductContract for inventory retrieval

--- src/products/contract/product.contract.ts
+++ src/products/contract/product.contract.ts
@@ -3,4 +3,5 @@ import { Product, QueryParams } from "../interfaces/product.interface";
 
 export abstract class ProductContract {
   abstract getProducts(query: QueryParams): Promise<Partial<Product[]>>;
+  abstract getProductInventory(sku: string): Promise<Partial<Product>>;
 }
